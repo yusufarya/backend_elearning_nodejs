6dc6fe8bd1cbcf90a639ff00f733c4f2994eae01
yusufarya <aryaherby29nov2k@gmail.com>
2023-10-27 22:21:00 +0700

    update endpoint api.js

--- src/route/api.js
+++ src/route/api.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middleware/user-middleware.js";
 import userController from "../controller/user-controller.js";
-import masterGradeController from "../controller/master-grade-controller.js";
-import masterGradeCategoryController from "../controller/master-grade-category-controller.js";
-import masterMajorController from "../controller/master-major-controller.js";
+import M_MajorController from "../controller/master-major-controller.js";
+import M_GradeController from "../controller/master-grade-controller.js";
+import M_CategoryController from "../controller/master-grade-category-controller.js";
+import M_SubjectController from "../controller/master-subject-controller.js";
 
 const userRouter = new express.Router();
 userRouter.use(authMiddleware);
@@ -14,33 +15,33 @@ userRouter.patch("/api/users/current", userController.update);
 userRouter.delete("/api/users/logout", userController.logout);
 
 // MASTER //
+
 // major data api (Data jurusan) //
-userRouter.get("/api/major", masterMajorController.getAll);
-userRouter.post("/api/addMajor", masterMajorController.create);
-userRouter.get("/api/getMajor/:id", masterMajorController.getId);
-userRouter.patch("/api/updateMajor/:id", masterMajorController.update);
-userRouter.delete("/api/deleteMajor", masterMajorController.remove);
+userRouter.get("/api/major", M_MajorController.getAll);
+userRouter.post("/api/addMajor", M_MajorController.create);
+userRouter.get("/api/getMajor/:id", M_MajorController.getId);
+userRouter.patch("/api/updateMajor/:id", M_MajorController.update);
+userRouter.delete("/api/deleteMajor", M_MajorController.remove);
 
 // grade data api (Data Kelas) //
-userRouter.get("/api/grade", masterGradeController.getAll);
-userRouter.post("/api/addGrade", masterGradeController.create);
-userRouter.get("/api/getGrade/:id", masterGradeController.getId);
-userRouter.patch("/api/updateGrade/:id", masterGradeController.update);
-userRouter.delete("/api/deleteGrade/", masterGradeController.remove);
+userRouter.get("/api/grade", M_GradeController.getAll);
+userRouter.post("/api/addGrade", M_GradeController.create);
+userRouter.get("/api/getGrade/:id", M_GradeController.getId);
+userRouter.patch("/api/updateGrade/:id", M_GradeController.update);
+userRouter.delete("/api/deleteGrade/", M_GradeController.remove);
 
 // grade category data api (Data Kategori Kelas) //
-userRouter.get("/api/gradeCategory", masterGradeCategoryController.getAll);
-userRouter.post("/api/addGradeCategory", masterGradeCategoryController.create);
-userRouter.get(
-  "/api/getGradeCategory/:id",
-  masterGradeCategoryController.getId
-);
-userRouter.patch(
-  "/api/updateGradeCategory/:id",
-  masterGradeCategoryController.update
-);
-userRouter.delete(
-  "/api/deleteGradeCategory",
-  masterGradeCategoryController.remove
-);
+userRouter.get("/api/gradeCategory", M_CategoryController.getAll);
+userRouter.post("/api/addGradeCategory", M_CategoryController.create);
+userRouter.get("/api/getGradeCategory/:id", M_CategoryController.getId);
+userRouter.patch("/api/updateGradeCategory/:id", M_CategoryController.update);
+userRouter.delete("/api/deleteGradeCategory", M_CategoryController.remove);
+
+// major data api (Data jurusan) //
+userRouter.get("/api/subjects", M_SubjectController.getAll);
+userRouter.post("/api/addSubject", M_SubjectController.create);
+userRouter.get("/api/getSubject/:id", M_SubjectController.getId);
+userRouter.patch("/api/updateSubject/:id", M_SubjectController.update);
+userRouter.delete("/api/deleteSubject", M_SubjectController.remove);
+
 export { userRouter };
